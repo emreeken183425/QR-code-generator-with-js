348021cb3d2f984f47a046843be87ab95a15cfa8
emreeken183425 <emreeken1834@gmail.com>
2022-07-05 16:51:10 +0300

    loading

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function generateQR(){
     document.querySelector("#qr-image").style.display="black";
     let QRtext=document.querySelector("#text").value;
     if(QRtext.trim().length=0){
+
         document.querySelector("#qr-image .error ").innerHTML="please enter text";
         document.querySelector("#img").style.display="none";
+        
     }else{
         document.querySelector("#img").style.display="block";
         document.querySelector("#qr-image .error").innerHTML="";
-        document.querySelector("#img").src="" +QRtext;
+        document.querySelector("#img").src="https://emreeken183425.github.io/QR-code-generator-with-js/" +QRtext;
     }
 
 }
